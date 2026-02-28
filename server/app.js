aa5de7cc3f8e581695eb9ed5e0335e01f544fdb7
DovyMay <dovydasmajauskas1@gmail.com>
2026-02-28 16:16:07 +0100

    Routes done

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ const PORT = 5005;
 const cors = require("cors");
 const mongoose = require("mongoose");
 const StudentModel = require("./models/StudentModel");
+const CohortModel = require("./models/CohortModel")
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
@@ -44,6 +45,7 @@ app.get("/docs", (req, res) => {
 //Get all students
 app.get("/api/students", (req, res) => {
   StudentModel.find({})
+  .populate('cohort')
   .then((students) => {
     console.log("Students incoming", students);
     res.status(200).json(students);
@@ -56,10 +58,10 @@ app.get("/api/students", (req, res) => {
 
 //Get all students of specific cohort?
 app.get("/api/students/cohort/:cohortId", (req, res) => {
-  StudentModel.findById(req.params.cohortId)
-  .then((allCohortStudents) => {
-    console.log("Students incoming", allCohortStudents);
-    res.status(200).json(allCohortStudents);
+  StudentModel.find({cohort: req.params.cohortId})
+  .then((cohortStudents) => {
+    console.log("Students incoming", cohortStudents);
+    res.status(200).json(cohortStudents);
   })
   .catch((err) => {
       console.log(err);
@@ -69,7 +71,7 @@ app.get("/api/students/cohort/:cohortId", (req, res) => {
 
 //Returns the specified student by id
 app.get("/api/students/:studentId", (req, res) => {
-  StudentModel.findById(req.params.studentId)
+  StudentModel.findById(req.params.studentId).populate('cohort')
   .then((oneStudent) => {
     console.log("Student incoming", oneStudent);
     res.status(200).json(oneStudent);
@@ -80,7 +82,7 @@ app.get("/api/students/:studentId", (req, res) => {
     });
 })
 
-//Create new student
+//Create new student 
 app.post("/api/students", (req, res) => {
   StudentModel.create(req.body)
   .then(createdStudent => {
@@ -98,7 +100,7 @@ app.put("/api/students/:studentId", (req, res) => {
   StudentModel.findByIdAndUpdate(req.params.studentId, req.body, {new: true})
   .then(updatedStudent => {
     console.log("Updated student", updatedStudent)
-    res.status(200).json(updatedBook)
+    res.status(200).json(updatedStudent)
   })
   .catch((err) => {
       console.log(err);
@@ -106,17 +108,67 @@ app.put("/api/students/:studentId", (req, res) => {
     });
 })
 
-//Deletes the specified cohort by id
+//Deletes the specified student by id 
 app.delete("/api/students/:studentId", (req, res) => {
-  StudentModel.delete(req.params.studentId)
+  StudentModel.findByIdAndDelete(req.params.studentId)
   .then((result) => {
     console.log("Deleted!");
-    res.status(200).send("Student deleted");
+    res.status(200).send(result);
     })
     .catch((err) => {
       console.log(err);
       res.status(500).json(err);
     });
+})
+
+//Cohort routes
+//Create new one
+app.post("/api/cohorts" , async (req, res) => {
+  try {
+    const createdCohort = await CohortModel.create(req.body);
+    res.status(201).json(createdCohort);
+    console.log("Created")
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+})
+
+//Get all cohorts
+app.get("/api/cohorts", (req, res) => {
+  CohortModel.find({})
+  .then((cohorts) => {
+    console.log("Cohorts incoming", cohorts);
+    res.status(200).json(cohorts);
+  })
+  .catch((error) => {
+      console.error("Error while retrieving cohorts ->", error);
+      res.status(500).json({ error: "Failed to retrieve cohorts" });
+    });
+})
+
+//  Updating Cohort
+app.put("/api/cohorts/:cohortId", (req, res) => {
+  CohortModel.findByIdAndUpdate(req.params.cohortId, req.body, {new: true})
+  .then(updatedCohort => {
+    console.log("Updated cohort", updatedCohort)
+    res.status(200).json(updatedCohort)
+  })
+  .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+})
+
+// Delete Cohort
+app.delete("/api/cohorts/:cohortId" , async (req, res) => {
+  try{
+const deletedCohort = await CohortModel.findByIdAndDelete(req.params.cohortId)
+res.status(200).json(deletedCohort);
+  }catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 })
 
 
